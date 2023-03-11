cd450c39da33288fec66c0993d1539d523d42024
w1atrak <danielbielaszka@gmail.com>
2023-03-11 12:43:29 +0100

    some warnings and dynamic lib not working

---
+++ BielaszkaDaniel/cw01/zad3b/dynamic.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+typedef struct {
+    char** blocks;
+    int size;
+    int capacity;
+} WordCounters;
+
+
+
+
+
+WordCounters (*Lib1CreateStruct)(int);
+
+void (*Lib1Count)(WordCounters* , char* );
+
+char* (*Lib1GetBlock)(WordCounters* , int );
+
+void (*Lib1RemoveBlock)(WordCounters* , int );
+
+void (*Lib1DeleteBlocks)(WordCounters* );
+
+  
